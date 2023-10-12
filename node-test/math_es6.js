aba3b0abeab7d7c9225117cabced2b6d9bf04716
Aru428 <sohee5997@naver.com>
2023-10-12 13:43:24 +0900

    study: es6 모듈

---
+++ node-test/math_es6.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const PI = 3.14;
+
+// 1) 하나만 내보낼 경우
+// module.exports = add
+// export default add;
+
+// 2) 여러개 내보낼 경우
+export default PI;
+export { add, minus };
