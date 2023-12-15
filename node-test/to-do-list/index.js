a4b812e9919537a360e45c075e6c9edd8851345e
sohee <sohee5997@naver.com>
2023-12-15 22:57:43 +0900

    to-do-list: 투두 리스트 관련 라우터 생성

--- node-test/to-do-list/index.js
+++ node-test/to-do-list/index.js
@@ -10,6 +10,10 @@ app.use("/", router);
 const userRouter = require("./routes/user");
 app.use("/user", userRouter);
 
+// 투두 리스트 관련 라우터
+const tasksRouter = require("./routes/tasks");
+app.use("/tasks", tasksRouter);
+
 app.listen(PORT, function () {
   console.log(`Sever Open: ${PORT}`);
 });
